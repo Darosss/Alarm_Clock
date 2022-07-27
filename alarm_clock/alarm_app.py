7754ad22f411bb1724845c536e5bbd5bbb55b72e
Darosss <Daroing12@gmail.com>
2022-07-27 17:44:28 +0200

    "Added settings_popup to class"

--- alarm_clock/alarm_app.py
+++ alarm_clock/alarm_app.py
@@ -8,30 +8,6 @@ from timer import Timer
 # Alarm clock that I wanted to create based on android alam clock but for windows
 # Prototype ver 1.0 Kappa
 
-class MainMenu(tk.Menu):
-    @property
-    def root(self):
-        return self._root
-
-    def __init__(self, root, *args, **kwargs):
-        tk.Menu.__init__(self, root, *args, **kwargs)
-        self._root = root
-
-        window_menu = WindowMenu(self, tearoff=0)
-        self.add_cascade(label="Window", menu=window_menu)
-        
-        root.config(menu = self)
-
-class WindowMenu(tk.Menu):
-    """Creates Window menu."""
-    
-    def __init__(self, parent, *args, **kwargs):
-        tk.Menu.__init__(self, parent, *args, **kwargs)
-        
-        self.add_command(label="Upload", command=parent.root.upload_file)
-        self.add_command(label="Settings", command=parent.root.menu_settings)
-        self.add_command(label="Exit", command=parent.root.quit_app)
-
 
 class AlarmApp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -57,74 +33,87 @@ class AlarmApp(tk.Tk):
         self.rowconfigure(2, weight=1)
         # self.create_footer_app()
         # self.create_menu_app()
-        self.show_app(self._alarm_app_frame)
+        self.show_frame(self._alarm_app_frame)
+
+    def show_frame(self, show_what):
+        show_what.grid(column=0, row=1, columnspan=2, sticky="nsew")
 
     def upload_file(self):
         print('upload')
-        
+      
     def menu_settings(self):
-        def save_settings():
-            for slave in top.grid_slaves():
-                if slave.widgetName == 'entry':
-                    sect_and_key = slave.winfo_name().split("/")
-                    new_key = self.config_ini.get_key(sect_and_key[0], sect_and_key[1], True)
-                    new_key = new_key.replace(new_key.split("#")[0], slave.get())
-                    self.config_ini.save_config(sect_and_key[0], sect_and_key[1], new_key) 
-        def add_header_label(header_name):
-            row_grid = top.grid_size()[1] + 1
-            ttk.Label(top, text=header_name, justify='center', background="lightblue").grid(column=0, columnspan=2, row = row_grid, sticky="e")
+        window = SettingsWindow(self, self.config_ini)
 
-        def add_setting(sett_descr, section_name, option_name, row, width=20):
-            row_grid = top.grid_size()[1] + row
-            ttk.Label(top, text=sett_descr, background="lightblue").grid(column=0, row = row_grid, sticky="nse")
-            res_entry = tk.Entry(top, width=width, name=f"{section_name}/{option_name}")
-            res_entry.insert(0, self.config_ini.get_key(section_name, option_name))
-            res_entry.grid(column = 1, row = row_grid, sticky="nsew")
-
-        def write_config_settings(section_name):
-            alarms_appearance = self.config_ini.get_sections_keys(section_name, False)
-            add_header_label(section_name)
-            for index, sett in enumerate(alarms_appearance):
-                sett_split = sett.split("/")
-                sett_key_name = sett_split[0]
-                sett_descrip = sett_split[1].split("#")[1]
-                add_setting(sett_descrip, section_name, sett_key_name, index)
-                
-        top = tk.Toplevel(self)
-        top.geometry("750x250")
-        top.title('Settings')
-        top.columnconfigure(0, weight=1)
-        top.columnconfigure(1, weight=1)
-        top.columnconfigure(2, weight=1)
-
-
-        save_btn = ttk.Button(top, text="Save")
-        save_btn.grid(column=2, row=0)
-        save_btn.config(command = lambda : save_settings())
-        
-        for section in self.config_ini.get_all_sections():
-            if not section[0] == "_":
-                write_config_settings(section)
-        
     def quit_app(self):
         print('quit')
 
-    def create_scrollbar(self):
-        pass
 
-    def create_timer_app(self):
-        self._timer_app_frame = ttk.Frame(self, style=self.styleName, name='timer_app')
-        self._timer_app_frame.columnconfigure(0, weight=1)
-        self._timer_app_frame.columnconfigure(1, weight=1)
-        self._timer_app_frame.rowconfigure(0, weight=1)
+class SettingsWindow(tk.Tk):
+    def __init__(self, root, config_settings, *args, **kwargs):
+        tk.Toplevel.__init__(self, *args, **kwargs)
+        self.config_sett = config_settings
+        self.geometry("750x250")
+        self.title('Settings')
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+        save_btn = ttk.Button(self, text="Save")
+        save_btn.grid(column=2, row=0)
+        save_btn.config(command = lambda : self.save_settings())
+        
+        for section in self.config_sett.get_all_sections():
+            if not section[0] == "_":
+                self.write_config_settings(section)
+        
+    def save_settings(self):
+        for slave in self.grid_slaves():
+            if slave.widgetName == 'entry':
+                sect_and_key = slave.winfo_name().split("/")
+                new_key = self.config_sett.get_key(sect_and_key[0], sect_and_key[1], True)
+                new_key = new_key.replace(new_key.split("#")[0], slave.get())
+                self.config_sett.save_config(sect_and_key[0], sect_and_key[1], new_key) 
+    def add_header_label(self, header_name):
+        row_grid = self.grid_size()[1] + 1
+        ttk.Label(self, text=header_name, justify='center', background="lightblue").grid(column=0, columnspan=2, row = row_grid, sticky="e")
 
-        timer = Timer(self.config_name, self.styleName)
-        timer.create_timer_frame(self._timer_app_frame)
+    def add_setting(self, sett_descr, section_name, option_name, row, width=20):
+        row_grid = self.grid_size()[1] + row
+        ttk.Label(self, text=sett_descr, background="lightblue").grid(column=0, row = row_grid, sticky="nse")
+        res_entry = tk.Entry(self, width=width, name=f"{section_name}/{option_name}")
+        res_entry.insert(0, self.config_sett.get_key(section_name, option_name))
+        res_entry.grid(column = 1, row = row_grid, sticky="nsew")
 
-        return self._timer_app_frame
+    def write_config_settings(self, section_name):
+        alarms_appearance = self.config_sett.get_sections_keys(section_name, False)
+        self.add_header_label(section_name)
+        for index, sett in enumerate(alarms_appearance):
+            sett_split = sett.split("/")
+            sett_key_name = sett_split[0]
+            sett_descrip = sett_split[1].split("#")[1]
+            self.add_setting(sett_descrip, section_name, sett_key_name, index)
+    
+class MainMenu(tk.Menu):
+    @property
+    def root(self):
+        return self._root
 
-    def show_app(self, show_what):
-        show_what.grid(column=0, row=1, columnspan=2, sticky="nsew")
+    def __init__(self, root, *args, **kwargs):
+        tk.Menu.__init__(self, root, *args, **kwargs)
+        self._root = root
+
+        window_menu = WindowMenu(self, tearoff=0)
+        self.add_cascade(label="Options", menu=window_menu)
+        
+        root.config(menu = self)
+
+class WindowMenu(tk.Menu):
+    """Creates Window menu."""
+    def __init__(self, parent, *args, **kwargs):
+        tk.Menu.__init__(self, parent, *args, **kwargs)
+        
+        self.add_command(label="Upload", command=parent.root.upload_file)
+        self.add_command(label="Settings", command=parent.root.menu_settings)
+        self.add_command(label="Exit", command=parent.root.quit_app)
 
 
 class AlarmsFrame(tk.Frame):
@@ -188,13 +177,8 @@ class FooterFrame(tk.Frame):
         tk.Frame.__init__(self, root, background='blue', *args, **kwargs)
         self.time_label = ttk.Label(self, justify='center',
                                font=('calibri', 25, 'bold'), borderwidth=1, relief="solid")
-        
-        # button_setting = ttk.Button(self.footer_frame, text="SETTINGS")
-        # button_setting.config(command=self.setting_window_popup)
-        # watch this will be changed for down timer or upper i mean for every other application
         self.time()
 
-        # button_setting.grid(column=0, row=0, sticky="nsw")
         self.time_label.grid(column=2, row=0, sticky='nsew')
         self.grid(column=0, row=2, columnspan=2, sticky="nsew")
 
