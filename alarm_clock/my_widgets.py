f0f77af503c13afd81cb3f3ce4244a341f99acab
Darosss <Daroing12@gmail.com>
2022-08-27 19:33:45 +0200

    Added my option menu and used it in edit alarm

--- alarm_clock/my_widgets.py
+++ alarm_clock/my_widgets.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from turtle import back
+from tkinter import ttk
 
 
 class MyButton(tk.Button):
@@ -49,3 +49,35 @@ class MyEntry(tk.Frame):
         )
         self.entry.insert(0, value_entr)
         self.entry.pack()
+
+
+class MyOptionMenu(tk.Frame):
+    def __init__(self, master, fg='black', bg='white', title='', title_img='',
+                 font='default', font_size='10', img_option='', value='', *values,
+                 ** options):
+        tk.Frame.__init__(
+            self, master,
+            background=bg
+        )
+        if len(title) > 0:
+            self.title = MyLabel(self, title, fg, bg,
+                                 image=title_img
+                                 )
+            self.title.pack(expand=True, fill='x')
+        s = ttk.Style()
+        s.configure(
+            "my.TMenubutton",
+            font=(font, font_size),
+            image=img_option,
+            background=bg,
+            foreground=fg,
+            compound=tk.CENTER,
+        )
+        self.option_menu = ttk.OptionMenu(
+            self,
+            value,
+            "",
+            *values,
+            style="my.TMenubutton",
+        )
+        self.option_menu.pack()
